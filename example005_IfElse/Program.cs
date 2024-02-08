dd81df2a31827d4684200bb91fa1f3642ee558f3
Viktor Menshenin <vix@Viktors-MBP.mshome.net>
2024-02-08 17:26:46 +0500

    Example 005 IfElse - added

---
+++ example005_IfElse/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите ваше имя");
+string userName = Console.ReadLine();
+if(userName.ToLower() == "маша"){
+    Console.WriteLine("Ура это Маша!");
+}else{
+    Console.Write("Привет, ");
+    Console.WriteLine(userName);
+}
